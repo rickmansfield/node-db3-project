6b8309b52ba2b57d92690948d43849a0518c202b
Rick <ricksmycodeguy@gmail.com>
2021-10-06 18:18:22 -0500

    MIDDLEWARE - ValidateScheme wired working 5x5

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -12,14 +12,14 @@ const checkSchemeId = async (req, res, next) => {
     const existing = await db('schemes')
       .where('scheme_id', req.params.scheme_id)
       .first();
-      if (!existing){
-        next({
-          status: 404,
-          message: `scheme with scheme_id ${req.params.scheme_id} not found`
-        })
-      } else{
-        next()
-      }
+    if (!existing) {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`
+      })
+    } else {
+      next()
+    }
   } catch (err) {
     next(err)
   }
@@ -34,7 +34,19 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const { scheme_name } = req.body
+  if (
+    scheme_name === undefined ||
+    typeof scheme_name !== 'string' ||
+    !scheme_name.trim()
+  ) {
+    next({
+      status: 400,
+      message: 'invalid scheme_name'
+    })
+  } else {
+    next()
+  }
 }
 
 /*
